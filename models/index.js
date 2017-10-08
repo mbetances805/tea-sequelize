85c6dca4ffaa324822be72c62411a276e7d841b9
Maria Betances <mbetances1002@gmail.com>
2017-10-08 12:13:49 -0400

    completed exercises

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 'use strict'
 
-const Sequelize = require('sequelize');
+const Sequelize = require('sequelize')
+const Op = Sequelize.Op
 
 const db = new Sequelize('postgres://localhost/teas', { logging: false });
 
@@ -10,7 +11,38 @@ const Tea = db.define('tea', {
   price: Sequelize.INTEGER,
   category: Sequelize.ENUM('green', 'black', 'herbal')
 }, {
-  // add more functionality to our Tea model here!
-})
+    getterMethods: {
+      dollarPrice () {
+        let price = '$' + (this.price/100).toFixed(2).toString()
+        return price
+      }
+    },
+    hooks: {
+      beforeCreate: (instance, options) => {
+       instance.title = instance.title.split(' ').map((word) => {
+         return (word.charAt(0).toUpperCase() + word.substr(1))
+       }).join(' ')
+      }
+    }
+  })
+
+Tea.findByCategory = (type) => {
+  return Tea.findAll({
+    where: {
+      category: type
+    }
+  })
+}
+
+Tea.prototype.findSimilar = function() {
+  return Tea.findAll({
+    where: {
+      category: this.category,
+      title: {
+        [Op.ne]: this.title
+      }
+    }
+  })
+}
 
 module.exports = { db, Tea };
